59c3fcf67327eb5bba1ff84e7a8228833e2573a8
yukitotora <yukitotora@gmail.com>
2020-08-26 17:00:11 +0900

    static関連のsettings.pyを修正

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "blog/static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
